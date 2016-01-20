d17ccb21932fef66cf1f514b16155275b8e89fe8
zeeamber <zee.amb@gmail.com>
2016-01-21 03:09:14 +0530

    views removed

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+    res.send("Welcome!");
 });
 
 /* Login */
 router.get('/login', function(req, res) {
-    res.render('login', {user : req.user});
+    //res.render('login', {user : req.user});
 });
 
 router.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
+    //req.logout();
+    //res.redirect('/');
 });
 
 module.exports = router;
