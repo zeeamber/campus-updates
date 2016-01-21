50d78e15b0a2ea61fd10f699a3211f8cfb1e0c65
zeeamber <zee.amb@gmail.com>
2016-01-21 14:15:23 +0530

    socket config

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var util = require('util');
+var WebSocketServer = require("ws").Server
 
 var mongoose = require('mongoose');
 require('./models/feeds');
@@ -22,7 +23,8 @@ var auth = require('./routes/auth');
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+server.listen(3000);
+/*var io = require('socket.io').listen(server);
 server.listen(3000);
 io.on('connection', function(socket) {
     socket.on('new_status', function(status) {
@@ -32,7 +34,22 @@ io.on('connection', function(socket) {
         feed.save();
         socket.emit('new_update', status);
     });
-});
+});*/
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
+
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
