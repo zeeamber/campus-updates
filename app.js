3e6265a4950f0c24ecc86d80b44a0277940d186d
zeeamber <zee.amb@gmail.com>
2016-01-21 14:18:20 +0530

    socket config

--- app.js
+++ app.js
@@ -38,15 +38,15 @@ io.on('connection', function(socket) {
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
-wss.on("connection", function(ws) {
+wss.on('connection', function(ws) {
   var id = setInterval(function() {
     ws.send(JSON.stringify(new Date()), function() {  })
   }, 1000)
 
-  console.log("websocket connection open")
+  console.log('websocket connection open')
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
+  ws.on('close', function() {
+    console.log('websocket connection close')
     clearInterval(id)
   })
 })
